e3a970592c54ba70b982d7266e69d3305b643386
neuekaroly <karolyneue6@gmail.com>
2018-03-11 16:41:31 +0100

    Costumize the marker icon, and an example of marker.

--- app/src/main/java/com/example/neuekaroly/ehubsharing/MapActivity.java
+++ app/src/main/java/com/example/neuekaroly/ehubsharing/MapActivity.java
@@ -1,6 +1,8 @@
 package com.example.neuekaroly.ehubsharing;
 
 import android.Manifest;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -19,8 +22,6 @@ import java.util.LinkedList;
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
-    private static LinkedList<LatLng> mapMarkers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +38,24 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 
         mMap.getUiSettings().setZoomControlsEnabled(true);
 
+        addMarkersToMap();
+
         LatLng southMap = new LatLng(47.024250, 15.834115);
         LatLng westMap = new LatLng(47.908052, 23.211434);
 
         LatLngBounds mapBounds = new LatLngBounds(southMap, westMap);
 
         mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(mapBounds, 40));
+    }
+
+    private void addMarkersToMap() {
+
+        Bitmap icon = BitmapFactory.decodeResource(this.getResources(), R.drawable.black_charger);
+
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(47.50569970,19.06325010))
+                .title("Oktogon 2.")
+                .snippet("06 1/238 1888")
+                .icon(BitmapDescriptorFactory.fromBitmap(icon)));
     }
 }
