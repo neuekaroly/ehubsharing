0ceb31b07d0eb50b49353f4d9b5a532db4cb4699
neuekaroly <karolyneue6@gmail.com>
2018-03-30 15:13:26 +0200

    Integrate Stetho.

--- app/src/main/java/com/example/neuekaroly/ehubsharing/SplashActivity.java
+++ app/src/main/java/com/example/neuekaroly/ehubsharing/SplashActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-import Entity.DaoSession;
+import com.facebook.stetho.Stetho;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -16,6 +16,8 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Stetho.initializeWithDefaults(this);
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
